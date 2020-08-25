e9a53f6c2be49504942f1307e8cb74e71627ac31
Shivam Sharma <kshivam654@gmail.com>
2020-08-25 19:56:47 +0530

    HBS dynamic web page templeting added and 404 page

---
+++ public/js/app.js
@@ -0,0 +1,1 @@
+console.log('clinet side js is loaded')
--- src/app.js
+++ src/app.js
@@ -2,20 +2,40 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
+
+//Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
+
+//Setup handlebar engine views locations
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
 
 app.use(express.static(publicDirectoryPath))
 
-app.get('/help', (req, res)=>{
-    res.send('Help Page')
+app.get('', (req, res)=>{
+    res.render('index', {
+        title: 'From A Static File From HBS (2)',
+        name: 'Shivam Sharma'
+    })
 })
 
 app.get('/about', (req, res)=>{
-    res.send('About Page')
+    res.render('about', {
+        title: 'About Page'
+    })
 })
 
 app.get('/weather', (req, res)=>{
-    res.send('Weather Page')
+    res.render('weather')
+})
+
+app.get('/help', (req, res)=>{
+    res.render('help')
+})
+
+app.get('*', (req, res)=>{
+    res.send('Eror 404 Page not Found')
 })
 
 app.listen(3000, ()=>{
